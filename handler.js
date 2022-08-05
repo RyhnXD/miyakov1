d344c6578fd9092320577bbfc4421b51aa67d78c
Kaguya Shinomiya <102699105+RyhnXD@users.noreply.github.com>
2022-08-05 13:45:26 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -551,7 +551,7 @@ esteh: 0,
                 if (!('isBanned' in chat))
                     chat.isBanned = false
                 if (!('welcome' in chat))
-                    chat.welcome = false
+                    chat.welcome = true
                 if (!('detect' in chat))
                     chat.detect = false
                 if (!('sWelcome' in chat))
